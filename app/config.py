c6148bf8f6278c6af0cca7dfbbe2ae18422a4d18
Bourne <xiaopei0206@icloud.com>
2024-06-27 15:15:34 +0800

    load config from env

--- app/config.py
+++ app/config.py
@@ -1,8 +1,17 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
+
+username = os.getenv('DB_USERNAME')
+password = os.getenv('DB_PASSWORD')
+hostname = os.getenv('DB_HOST', 'localhost')
+database_name = os.getenv('DB_NAME')
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqldb://{username}:{password}@{hostname}/{database_name}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'your_jwt_secret_key'
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=2)
