3e3b10045765fe52f9f83f507aaaa3edff9fb71b
Bourne <xiaopei0206@icloud.com>
2025-03-20 12:40:07 +0800

    fix cors

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app)
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "https://read.english-reader.com"
+        ],
+        "methods": ["GET", "POST", "OPTIONS"],
+        "allow_headers": ["Content-Type", "Authorization"]
+    }
+})
 
 if __name__ == '__main__':
     app.run(debug=True)
